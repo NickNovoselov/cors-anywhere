2571b3710588e5440f779c06180affc3b2bd8cf5
NickNovoselov <1499087+nicknovoselov@users.noreply.github.com>
2023-07-14 08:44:15 +0300

    increased server requestTimeout from 5 to 60 minutes

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ cors_proxy.createServer({
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
-    httpsOptions: {
-      requestTimeout: 60 * 60 * 1000
-    }
+  },
+  httpsOptions: {
+    requestTimeout: 60 * 60 * 1000
   },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
